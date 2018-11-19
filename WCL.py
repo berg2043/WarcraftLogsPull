bc7b2d7b1494a0af849b6471670d8008531c604f
berg2043 <44420714+berg2043@users.noreply.github.com>
2018-11-19 13:16:02 -0600

    Local Pull
    Pull to local file to experiment on without having to constantly be pulling the same data.

--- WCL.py
+++ WCL.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-apikey = '&api_key=<API KEY HERE>'
+apikey = '&api_key=81e7e6e4402a160beb16a24e735d62b4'
 url = 'https://www.warcraftlogs.com:443/v1/'
 call = 'rankings/encounter/'
 id = '2144'
@@ -13,10 +13,12 @@ page = '&page=' + str(numb)
 
 r = requests.get(url + call + id + metric + dif + region + page + apikey)
 thing = r.json()
+with f = open('data.json','w') as outfile:
+  json.dump(data, outfile)
 
-def pull(x):
-  for n in x:
-    print(n['name'])
-    print(n['total'])
+#def pull(x):
+#  for n in x:
+#    print(n['name'])
+#    print(n['total'])
 
-pull((thing['rankings']))
+#pull((thing['rankings']))
